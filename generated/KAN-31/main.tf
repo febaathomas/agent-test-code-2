a42810de5f49344352145bc2e080e8fe3b4f00e9
febaathomas <131870140+febaathomas@users.noreply.github.com>
2026-05-12 10:23:28 +0530

    feat: KAN-31 terraform

---
+++ generated/KAN-31/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "kan_31_ec2" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "KAN-31-ec2"
+  }
+}

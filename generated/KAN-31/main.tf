1d990ebe6f1feb99ae7dd608c58623fd0ca137af
febaathomas <131870140+febaathomas@users.noreply.github.com>
2026-05-12 12:34:05 +0530

    feat: KAN-31 terraform

--- generated/KAN-31/main.tf
+++ generated/KAN-31/main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "kan_31_ec2" {
+resource "aws_instance" "example" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
   instance_type = "t2.micro"
 
